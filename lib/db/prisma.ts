1bf90ed2fd0021345d4a2fa8047198978899f729
manunova1234 <manohar.lotlapalli@novalink-solutions.com>
2026-02-17 14:41:24 +0530

    fix

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+function createPrismaClient() {
+  return new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
